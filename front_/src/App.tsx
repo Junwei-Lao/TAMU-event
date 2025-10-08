99ced37435ce609b40fe470ddea5e53bbaf55804
Junwei-Lao <junwei.lao.0810@gmail.com>
2025-10-08 18:30:50 -0500

    Fixed some frontend texts

--- front_/src/App.tsx
+++ front_/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <DeepChat
           className='dc-container'
           style={{width: '98%', height: '93vh'}}
-          introMessage={{text: 'Welcome! 👋 \n \n Tell us what events you’re interested in — short or detailed, we’ll do our best to help! We’ll search for up to 20 best-matching events based on what you send us.\n \n 💡 *Try sending: What math events are coming up in the future?* \n \n To explore more features, send **/commands** \n \n To report any issues, contact **junweilao@tamu.edu** \n \n *Note: For your privacy, chat history will be cleared when you refresh the page.* '}}
+          introMessage={{text: 'Welcome! 👋 \n \n Tell us what events you’re interested in — short or detailed, we’ll do our best to help! We’ll search for up to 20 best-matching events based on what you send us.\n \n 💡 *Try sending: What events related to building a good resume are coming up in the future?* \n \n To explore more features, send **/commands** \n \n To report any issues, contact **junweilao@tamu.edu** \n \n *Note: For your privacy, chat history will be cleared when you refresh the page.* '}}
           connect={{
             url: '/api/chat',
             method: 'POST',
